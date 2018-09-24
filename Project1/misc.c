68089d0eb9fc8c21d6fbe498ca072304429b1d11
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2018-09-24 07:44:39 -0400

    starting some program logic

--- Project1/misc.c
+++ Project1/misc.c
@@ -37,7 +37,69 @@ char* readFile(char *relativePath) {
 }
 
 
+char** str_split(char* a_str, const char a_delim) {
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
 
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+
+void fillConf(struct Conf* conf, char* key, char* val) {
+	// sorry for the dirty code :)
+	if(!strcmp(key, "SEED")) {conf->SEED = atoi(val);return;}
+	if(!strcmp(key, "INIT_TIME")) {conf->INIT_TIME = atoi(val);return;}
+	if(!strcmp(key, "FIN_TIME")) {conf->FIN_TIME = atoi(val);return;}
+	if(!strcmp(key, "ARRIVE_MIN")) {conf->ARRIVE_MIN = atoi(val);return;}
+	if(!strcmp(key, "ARRIVE_MAX")) {conf->ARRIVE_MAX = atoi(val);return;}
+	if(!strcmp(key, "QUIT_PROB")) {conf->QUIT_PROB = atoi(val);return;}
+	if(!strcmp(key, "CPU_MIN")) {conf->CPU_MIN = atoi(val);return;}
+	if(!strcmp(key, "CPU_MAX")) {conf->CPU_MAX = atoi(val);return;}
+	if(!strcmp(key, "DISK1_MIN")) {conf->DISK1_MIN = atoi(val);return;}
+	if(!strcmp(key, "DISK1_MAX")) {conf->DISK1_MAX = atoi(val);return;}
+	if(!strcmp(key, "DISK2_MIN")) {conf->DISK2_MIN = atoi(val);return;}
+	if(!strcmp(key, "DISK2_MAX")) {conf->DISK2_MAX = atoi(val);return;}
+}
 
 struct Conf readConf(char *relativePath) {
 	struct Conf conf;
@@ -45,24 +107,18 @@ struct Conf readConf(char *relativePath) {
 	
 	char *confText = readFile(relativePath);
 	
-	char key [30];
-	char val [30];
-	size_t order = 0;
+	char **lines = str_split(confText,'\n');
+	if(lines) {
+		for (int i = 0; *(lines + i); ++i) {
+			char **kv = str_split(*(lines + i),' '); // split key-value
+			if(kv[0] && kv[1]) {
+				fillConf(&conf, kv[0], kv[1]);
+			}
 
-	size_t i = 0, k = 0;
-	char ch;
-	while(ch = confText[i], ch != '\0') {
-		switch (order) {
-			case 0:
-				if(ch == ' ') {order = 1; k=0;}
-				else {key[k++] = ch;}
-				break;
-			case 1:
-				if(ch == '\n') {order = 0; k=0;}
-				else {val[k++] = ch;}
-				break;
-		}
-
-		++i;
+            free(*(lines + i));
+        }
 	}
+
+
+	return conf;
 }
--- Project1/misc.h
+++ Project1/misc.h
@@ -6,17 +6,46 @@ struct Conf
     uint SEED;
     uint INIT_TIME;
     uint FIN_TIME;
+
+    // jobs enter the system with an interarrival time that is uniformly distributed
     uint ARRIVE_MIN;
     uint ARRIVE_MAX;
     uint QUIT_PROB;
+    
+    // use the least busy disk when I/O is requested, as if in RAID 1 drive configuration, all disks contain the same info
+    // if the disk queues are of equal length, choose one of the disk at random.
+
+    /*
+    When jobs reach some component (CPU, disk1, or disk2), if that component is free, 
+    the job begins service there immediately. If, on the other hand, that component is 
+    busy servicing someone else, the job must wait in that component's queue.
+    */
+
+    // The queue for each system is FIFO
+
+    // determine burst time randomly
+
+    // a job is serviced by a component for an interval of time that is uniformly distributed
     uint CPU_MIN;
     uint CPU_MAX;
     uint DISK1_MIN;
     uint DISK1_MAX;
     uint DISK2_MIN;
     uint DISK2_MAX;
+    
+
+    // FIFO QUEUES:
+    /*
+        CPU
+        disk1
+        disk2
+        priority queue (to store events)
+    */
+    //an event might be something like "a new job entered the system", "a disk read finished", "a job finished at the CPU", etc. 
 };
-
-char* readFile(char *relativePath);
-
 struct Conf readConf(char *relativePath);
+
+struct Process
+{
+    
+};
