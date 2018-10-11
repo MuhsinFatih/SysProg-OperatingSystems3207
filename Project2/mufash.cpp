e7563947dc27d97c6152d0168dfb27f47fb555b5
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2018-10-10 22:36:04 -0400

    lexer done, PATH fetched done

--- Project2/mufash.cpp
+++ Project2/mufash.cpp
@@ -8,7 +8,7 @@
 #include <fstream>
 #include <string>
 #include <sys/ioctl.h>
-
+#include <boost/filesystem.hpp>
 
 #include <vector>
 #include <map>
@@ -29,31 +29,57 @@
 #include "built_in.hpp"
 
 
-using namespace std;
+#include <readline/readline.h>
+#include <readline/history.h>
 
+using namespace std;
+namespace fs = boost::filesystem;
 
 
 char* USER;
 char* HOSTNAME;
 string PATH;
-char* PWD;
+string CWD;
 extern char** environ; // the last item in the array is a NULL c-string
+std::vector<string> executables;
+
 
 string _user;
 string _host;
 string _promptArrow;
-void prompt() {
-    printf("%s@%s[%s] %s ", _user.c_str(), _host.c_str(), PWD, _promptArrow.c_str());
+
+char promptBuffer[PATH_MAX];
+
+char* prompt() {
+    sprintf(promptBuffer, "%s@%s[%s] %s ", _user.c_str(), _host.c_str(), CWD.c_str(), _promptArrow.c_str());
+    cout << promptBuffer;
+    return "";
+}
+
+void fetchExecutables() {
+    vs dirs = str_split(PATH, ":");
+    for(string &d : dirs) {
+        for (auto &p : fs::directory_iterator(d)) {
+            try {
+                if(!fs::is_directory(p))
+                    executables.push_back(p.path().string());
+            } catch(fs::filesystem_error &e) { 
+                if(e.code() != boost::system::errc::permission_denied)
+                    printf("%s: %s\n", p.path().c_str(), e.code().message().c_str());
+            }
+        }
+    }
 }
 
 void fetchEnviron() {
     bool debug = true;
     if(debug) {
-        PWD = "/Users/muhsinfatih/Documents/CourseMaterial/CIS 3207 - Systems Programming and Operating Systems/Assignments/Project2";
+        CWD = "/Users/muhsinfatih/Documents/CourseMaterial/CIS 3207 - Systems Programming and Operating Systems/Assignments/Project2";
         USER = "muhsinfatih";
         HOSTNAME = "mufa-mbp";
+        PATH = "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
     } else {
-        PWD = std::getenv("PWD");
+        CWD = std::getenv("PWD");
         USER = std::getenv("USER");
         HOSTNAME = (char*)malloc(80 * sizeof(char));
         if(gethostname(HOSTNAME,80)) {
@@ -66,7 +92,9 @@ void fetchEnviron() {
             }
             if(dotPos) {HOSTNAME[dotPos] = '\0'; free(*(&HOSTNAME + dotPos));}
         }
+        PATH = std::getenv("PATH");
     }
+    fetchExecutables();
     _user = YELLOW; _user.append(USER); _user.append(RESET);
     _host = MAGENTA; _host.append(HOSTNAME); _host.append(RESET);
     _promptArrow = GREEN "➜" RESET;
@@ -95,7 +123,7 @@ void built_in_func(int argc, vs argv) {
             if(chdir(path.c_str()) == 0) {
                 fprintf(stderr, "cd: %s: %s\n", strerror(errno), argv[1].c_str());
             } else {
-                PATH = getcwd_string();
+                CWD = getcwd_string();
             }
             break;
         }
@@ -147,19 +175,25 @@ int main(int argc, char** argv) {
         {"quit", command::quit}
     };
 
-
+    char* buf;
     std::string cmd_line; // string to store next command line
-    while(true) {
-        prompt();
+    while ((buf = readline(prompt())) != nullptr) {
+        if (strlen(buf) > 0) {
+            add_history(buf);
+        }
 
-        std::getline(std::cin, cmd_line);           // get command line
+        //
+        cmd_line = std::string(buf);
+        // std::getline(std::cin, cmd_line);           // get command line
         vs str_argv = str_split(cmd_line, " ");     // split command line to arguments
         if(!str_argv.size()) continue;
         char** cmd_argv = vs_to_ch(str_argv);
         
-/////////////////////
         string cmd = str_argv[0];
+
         
+        // lexer ##########################3
+
         vector<exec> cmds;
         exec first = {
             .executable_path = "",
@@ -177,21 +211,20 @@ int main(int argc, char** argv) {
         size_t c = cmd_line.length();
 
         bool ignorespace = false;
-
         for(int i=0; i<c; ++i) {
             char cur = cmd_line[i];
             struct exec *curCmd = &cmds[currentCMD];
             
             if(isvalid(cur)) {
                 ignorespace = false;
-                printf("Valid Char: %c\n", cur);
+                // printf("Valid Char: %c\n", cur);
                 curCmd->args[currentArg] += cur;
             } else {
                 if(cur == ' ') {
                     if(ignorespace) { continue;}
                     ignorespace = true;
                     curCmd->args.push_back("");
-                    printf("New Arg %s\n", curCmd->args[currentArg].c_str());
+                    // printf("New Arg %s\n", curCmd->args[currentArg].c_str());
                     ++currentArg;
                 } else if(cur == '|' || cur == ';') {
                     ignorespace = true;
@@ -208,37 +241,61 @@ int main(int argc, char** argv) {
                         .args = (vs){""},
                         .is_piped =  false
                     });
+                } else if(cur == '\\') {
+                    // take next char no matter what
+                    assert(cmd_line.size() >= i+1);
+                    curCmd->args[currentArg] += cmd_line[++i];
                 } else if(cur == '\'') {
-                    /*size_t k = i;
-                    while(k<c && cmd_line[k]) {
-
-                    }*/
-                    printf("Start quote");
                     bool escape = false;
-                    for(;i<c;++i) {
+                    
+                    for(i++;i<c;++i) {
                         char cur2 = cmd_line[i];
                         if(cur2 == '\'' && !escape) {
                             break;
                         } else if(cur2 == '\\') {
                             escape = !escape;
                         } else {
+                            escape = false;
                             curCmd->args[currentArg] += cur2;
                         }
-                        printf("Quotas %s\n", curCmd->args[currentArg].c_str());                                                        
                     }
-                    
+                } else if(cur == '#') {
+                    for(i++;i<c;++i) {
+                        if(cmd_line[i] == '#') {
+                            break;
+                        }
+                    }
                 }
             }
-        }
+        } 
+
+
+        // bir önceki pipe referansı
+
+        // kaçıncı pipe da duruldu 
+
         for(size_t i=0; i<cmds.size(); ++i) {
             cout << "-----------------" << endl;
             printf("exec_path:%s\nargs:\n", cmds[i].executable_path.c_str());
             for(size_t k=0; k<cmds[i].args.size();++k) {
                 cout << cmds[i].args[k] << endl;
             }
+
+            // ilk argümanı al
+            // eğer path a benziyorsa tam yolu bul, // man realpath // RTFM :D
+            // eğer benzemiyorsa path enviroment ile bbinary i bulmaya çalış
+            // executable olduğundan emin ol
+            // ???
+            // profit
+
+            // step = 0
+            // cmds.size();
+            // 
+
             printf("is_piped:%i\n", cmds[i].is_piped);
         }
         continue;
+
         if(map_contains(built_in_commands, cmd)) {
             // command is a built-in function
             // built_in::run(str_argv.size(), str_argv);
@@ -266,7 +323,6 @@ int main(int argc, char** argv) {
             // status = WEXITSTATUS(status);
             // printf("child exited with code %d\n",status);
         }
-
     }
     return 0;
 }
